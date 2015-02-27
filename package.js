ca39538fae9c231d228e62e030d7f94547f6c79c
John Griffiths <john@uxgent.co>
2015-02-27 15:24:07 -0500

    Switch to git submodule.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'johnantoni:meteor-picturefill',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: ' A responsive image polyfill for <picture>, srcset, sizes, and more ~ packaged for Meteor',
-  git: 'git@github.com:johnantoni/meteor-picturefill.git',
+  git: 'https://github.com/johnantoni/meteor-picturefill.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.2');
-  api.addFiles('vendor/js/picturefill.js');
+  api.versionsFrom('1.0.1');
+  api.addFiles('picturefill/dist/picturefill.js', 'client');
 });
